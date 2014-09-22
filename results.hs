60c6bf0f74b225d4288ccd8f588a32317205723d
Aner Lucero <4rgento@gmail.com>
2014-09-22 16:37:29 -0300

    Problems 1-5

---
+++ results.hs
@@ -0,0 +1,19 @@
+-- 1
+-- 3
+elementAt (x:xs) y = (x:xs) !! (y-1)
+--elementAt [1,2,3] 2 --2	
+
+--4
+myLength [] = 0
+myLength (x:xs) = 1 + myLength xs
+
+--5
+
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+--6
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome [] = True
+isPalindrome [x] = True
+isPalindrome (x:xs) = 
