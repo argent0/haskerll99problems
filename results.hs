468caf149074d030f22f86ea4c74869c4b2eb9b7
Aner Lucero <4rgento@gmail.com>
2014-09-22 16:43:34 -0300

    Problem6

--- results.hs
+++ results.hs
@@ -16,4 +16,4 @@ myReverse (x:xs) = myReverse xs ++ [x]
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome [] = True
 isPalindrome [x] = True
-isPalindrome (x:xs) = 
+isPalindrome (x:xs) = (x == last xs) && (isPalindrome $ init xs)
