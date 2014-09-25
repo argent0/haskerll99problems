4787ff834e27f6a4844590af0a24ee2d5ee9870f
Aner Lucero <4rgento@gmail.com>
2014-09-25 09:13:29 -0300

    P-28: Sorting list of lists

--- results.hs
+++ results.hs
@@ -375,3 +375,46 @@ group (g:gs) (i:is)
 	solve_element k (1:gs) (i:is) = map ([[k]]++) (distribute gs (remove_item k (i:is)))
 	remove_item k (x:xs) = (takeWhile (/= k) (x:xs)) ++ (tail (dropWhile (/= k) (x:xs)))
 
+-- 8 Problem 28
+-- Sorting a list of lists according to length of sublists
+--
+-- a) We suppose that a list contains elements that are lists themselves. The
+-- objective is to sort the elements of this list according to their length.
+-- E.g. short lists first, longer lists later, or vice versa.
+--
+-- Example:
+--
+-- * (lsort '((a b c) (d e) (f g h) (d e) (i j k l) (m n) (o)))
+-- ((O) (D E) (D E) (M N) (A B C) (F G H) (I J K L))
+-- Example in Haskell:
+--
+-- Prelude>lsort ["abc","de","fgh","de","ijkl","mn","o"]
+-- Prelude>["o","de","de","mn","abc","fgh","ijkl"]
+-- b) Again, we suppose that a list contains elements that are lists themselves.
+-- But this time the objective is to sort the elements of this list according to
+-- their length frequency; i.e., in the default, where sorting is done
+-- ascendingly, lists with rare lengths are placed first, others with a more
+-- frequent length come later.
+--
+-- Example:
+--
+-- * (lfsort '((a b c) (d e) (f g h) (d e) (i j k l) (m n) (o)))
+-- ((i j k l) (o) (a b c) (f g h) (d e) (d e) (m n))
+-- Example in Haskell:
+--
+-- lfsort ["abc", "de", "fgh", "de", "ijkl", "mn", "o"]
+-- ["ijkl","o","abc","fgh","de","de","mn"]
+
+lsort :: [[a]] -> [[a]]
+lsort [] = []
+lsort (l:ls) = lsort([n | n <- ls, (cmp n l == LT)])++ [l] ++ lsort([m | m <- ls , (cmp m l /= LT)])
+	where cmp a b = compare (length a) (length b)
+
+lfsort :: [[a]] -> [[a]]
+lfsort [] = []
+lfsort (l:ls) = lfsort([n | n <- ls, (cmp n l == LT)])++ [l] ++ lfsort([m | m <- ls , (cmp m l /= LT)]) 
+	where
+      cmp a b = compare (length (filter_lenght (length a) (l:ls))) (length (filter_lenght (length b) (l:ls)))
+      filter_lenght l = filter (\ x -> ((==) l (length x)))
+
+-- vim: expandtab
